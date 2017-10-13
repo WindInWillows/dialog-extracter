da441404c18bff9490b5fa6f9782146e7dea6e62
zzy <zzy_xpy@163.com>
2017-10-13 17:20:05 +0800

    基本完成

--- extracter.py
+++ extracter.py
@@ -5,13 +5,17 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 MODELDIR = 'C:\ltp-data-v3.3.1\ltp_data'
-FILE_IN = 'data/rmdmy.txt'
+FILE_IN = 'data/in/rmdmy.txt'
+FILE_OUT = 'data/out/rmdmy.out'
 from pyltp import *
 
+# 话语开始标志
 DIALOG_FLAG = '：'
 
+# 封装语言模型可大大减少判断说话者的耗时
 class LanguageModel:
     def __init__(self):
+        print '开始载入语言模型...'
         self.segmentor = Segmentor()
         self.segmentor.load(os.path.join(MODELDIR, "cws.model"))
         self.postagger = Postagger()
@@ -22,11 +26,12 @@ class LanguageModel:
         self.recognizer.load(os.path.join(MODELDIR, "ner.model"))
         self.labeller = SementicRoleLabeller()
         self.labeller.load(os.path.join(MODELDIR, "srl"))
-        self.cached = None
+        self.cache = None
+        print '语言模型载入成功'
 
     def isSpeaker(self, sentence, name):
-        if self.cached != sentence:
-            self.cached = sentence
+        if self.cache != sentence:
+            self.cache = sentence
             self.words = self.segmentor.segment(sentence)
             self.postags = self.postagger.postag(self.words)
             self.arcs = self.parser.parse(self.words, self.postags)
@@ -39,19 +44,9 @@ class LanguageModel:
                     return True
         return False
 
-def explore(name):
-    count = 0
-    with open(FILE_IN) as fin:
-        while True:
-            line = fin.readline()
-            if not line:
-                break
-            if name not in line:
-                continue
-            count += 1
-    print count
-
+# 提取有对话的句子并连接
 def getDialogSentence(file):
+    print '开始抽取有对话的句子...'
     text = []
     with open(file) as fin:
         while True:
@@ -70,63 +65,40 @@ def getDialogSentence(file):
                     pre = l
             if DIALOG_FLAG in pre:
                 text.append(pre)
+    print '抽取句子完成'
     return text
 
+# 抽取人物对话
 def extracter(file, names):
-    # lines = []
-    # res = []
-    # pres = []
     lm = LanguageModel()
     text = getDialogSentence(file)
     lan = {}
+    print '开始抽取人物对话...'
     for name in names:
         lan[name] = []
-    print len(text)
     for sen in text:
         for name in names:
             if lm.isSpeaker(sen.split(DIALOG_FLAG)[0], name):
                 lan[name].append(sen)
+    print '抽取人物对话成功'
     return lan
 
-    # with open(file, 'r') as fin:
-    #     while True:
-    #         line = fin.readline()
-    #         if not line:
-    #             break
-    #         if name not in line or DIALOG_FLAG not in line:
-    #             continue
-    #         lst = line.split('%s' % DIALOG_FLAG)
-    #         pre = SentenceSplitter.split(lst[0])[-1]
-    #         dialog = lst[1]
-    #         if name not in pre or not lm.isSpeaker(pre, name):
-    #             continue
-    #         lines.append(line)
-    #         pres.append(pre)
-    #         res.append(dialog)
-    #
-    # com = ['%s：%s' % pd for pd in zip(pres, res)]
-    #
-    # with open(file.split('.')[0] + '.com', 'w') as com_file:
-    #     for l in com:
-    #         com_file.write('%s' % l)
-    # with open(file.split('.')[0] + '.phase', 'w') as phase_file:
-    #     for l in lines:
-    #         phase_file.write('%s' % l)
-    # with open(file.split('.')[0] + '.pre', 'w') as pre_file:
-    #     for l in pres:
-    #         pre_file.write('%s\n' % l)
-    # with open(file.split('.')[0] + '.dialog', 'w') as res_file:
-    #     for l in res:
-    #         res_file.write(l)
-    # return res
+def storeResultMap(resMap, file):
+    print '正在存储文件...'
+    with open(file, 'w') as fout:
+        for key in resMap:
+            fout.write('\n%s:%i\n' % (key, len(resMap[key])))
+            for sen in resMap[key]:
+                fout.write('%s\n'%sen)
+    print '存储文件成功'
+
+
+def showResultMap(resMap):
+    for key in resMap:
+        print '\n%s:\n%i' % (key, len(resMap[key]))
+        for sen in resMap[key]:
+            print sen
 
 if __name__ == '__main__':
-    # lst = extracter(FILE_IN, '侯亮平')
-    # print len(lst)
-    # explore('钟小艾')
-    # lst = getDialogSentence(FILE_IN)
-    # for i in lst:
-    #     print i
-
-    lan = extracter(FILE_IN, ['侯亮平', '钟小艾'])
-    print lan
+    resMap = extracter(FILE_IN, ['侯亮平', '钟小艾', '祁同伟', '李达康', '沙瑞金', '陈海','陈岩石', '易学习', '赵德汉', '高小琴', '陆亦可', '赵东来'])
+    storeResultMap(resMap, FILE_OUT)
